85dbefd4340e06cfb276fa52ed558beb744d34d3
Abdullah <amoosa@relisource.com>
2023-12-22 15:58:01 +0600

    Update features

--- pytest_with_options/test_math_func.py
+++ pytest_with_options/test_math_func.py
@@ -1,12 +1,16 @@
 import math_func
+import pytest
+import sys
 def test_add():
     assert math_func.add(7,3) == 10
     assert math_func.add(7) == 9
-
+    print("---Execution Successful---")
+@pytest.mark.skipif(sys.version_info < (3,12),reason="Unsupported Python version")
+@pytest.mark.number
 def test_product():
     assert math_func.product(5,5) == 25
     assert math_func.product(5) == 10
-
+@pytest.mark.strings
 def test_add_strings():
     result = math_func.add('Hello ', 'world')
 
