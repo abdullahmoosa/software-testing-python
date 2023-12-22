0ed9bfe515391aeff51244da08e6b72feabc304f
Abdullah <amoosa@relisource.com>
2023-12-22 15:12:30 +0600

    Update features

---
+++ pytest_with_options/test_math_func.py
@@ -0,0 +1,15 @@
+import math_func
+def test_add():
+    assert math_func.add(7,3) == 10
+    assert math_func.add(7) == 9
+
+def test_product():
+    assert math_func.product(5,5) == 25
+    assert math_func.product(5) == 10
+
+def test_add_strings():
+    result = math_func.add('Hello ', 'world')
+
+    assert result == 'Hello world'
+    assert type(result) is str 
+    assert 'Heldio' not in result
